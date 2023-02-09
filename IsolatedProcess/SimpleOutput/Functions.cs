733b16d71ddc9aafa61e57adec6504bdd10f6e37
Jialin Xin <jixin@microsoft.com>
2023-02-09 18:07:46 +0800

    Add an isolate sample

---
+++ IsolatedProcess/SimpleOutput/Functions.cs
@@ -0,0 +1,48 @@
+using System.Net;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Logging;
+
+namespace SimpleOutput
+{
+    public class Functions
+    {
+        private readonly ILogger _logger;
+
+        public Functions(ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger<Functions>();
+        }
+
+        [Function("Index")]
+        public HttpResponseData Index([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req, FunctionContext context, ILogger logger)
+        {
+            _logger.LogInformation("C# HTTP trigger function processed a request.");
+
+            var path = Path.Combine(context.FunctionDefinition.PathToAssembly, "../index.html");
+            var response = req.CreateResponse();
+            response.WriteString(File.ReadAllText(path));
+            response.Headers.Add("Content-Type", "text/html");
+            return response;
+        }
+
+        [Function("Negotiate")]
+        public static HttpResponseData Negotiate([HttpTrigger(AuthorizationLevel.Anonymous)] HttpRequestData req,
+        [SignalRConnectionInfoInput(HubName = "notification")] string connectionInfo)
+        {
+            var response = req.CreateResponse(HttpStatusCode.OK);
+            response.Headers.Add("Content-Type", "application/json");
+            response.WriteString(connectionInfo);
+            return response;
+        }
+
+        [Function("Notification")]
+        [SignalROutput(HubName = "notification")]
+        public SignalRMessageAction Notification([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req)
+        {
+            _logger.LogInformation("C# HTTP trigger function processed a request.");
+
+            return new SignalRMessageAction("newMessage", new object[] { $"[{DateTime.UtcNow}]{Guid.NewGuid()}" });
+        }
+    }
+}
